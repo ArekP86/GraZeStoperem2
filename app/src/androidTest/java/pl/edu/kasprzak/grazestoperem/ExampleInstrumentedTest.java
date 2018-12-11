f00ed5a35dc048ed37ac7cbeee7bfbe7b5ddd533
Piotr Marcinkiewicz <p.marcinkiew@samsung.com>
2018-12-11 14:19:38 +0100

    Small style fixes, app ID typo fixed
    1. Missing white space characters were added.
    2. Typo in application ID was fixed kasprazak -> kasprzak

--- app/src/androidTest/java/pl/edu/kasprzak/grazestoperem/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pl/edu/kasprzak/grazestoperem/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package pl.edu.kasprazak.grazestoperem;
+package pl.edu.kasprzak.grazestoperem;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("pl.edu.kasprazak.grazestoperem", appContext.getPackageName());
+        assertEquals("pl.edu.kasprzak.grazestoperem", appContext.getPackageName());
     }
 }
